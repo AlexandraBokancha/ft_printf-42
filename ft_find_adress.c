b199ecdc20dfaa1e15e25a541a800045dbf18b60
Alexandra Bokancha <albokanc@bess-f4r3s12.clusters.42paris.fr>
2023-11-28 15:13:19 +0100

    uploaded printf

---
+++ ft_find_adress.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_adress.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albokanc <albokanc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 12:02:04 by albokanc          #+#    #+#             */
+/*   Updated: 2023/11/28 14:28:57 by albokanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_find_adress(void *ptr)
+{
+	int					i;
+	int					count;
+	char				*base;
+	unsigned long long	p;
+
+	base = "0123456789abcdef";
+	i = 0;
+	count = 0;
+	p = (unsigned long long)ptr;
+	if (p == 0)
+		return (ft_putstr("(nil)"));
+	count += write(1, "0x", 2);
+	count += ft_putnbr_base(p, base);
+	return (count);
+}
